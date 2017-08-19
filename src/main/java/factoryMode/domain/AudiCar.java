40e4f405dffe0ba89453a065f5c37dd089a30b4e
xinchao.wang <wangxinchao.ok@163.com>
2017-08-19 14:00:59 +0800

    车类及接口

---
+++ src/main/java/factoryMode/domain/AudiCar.java
@@ -0,0 +1,11 @@
+package factoryMode.domain;
+
+/**
+ * Created by wangxc on 2017/8/19 13:13.
+ */
+public class AudiCar implements Car {
+    @Override
+    public void run() {
+        System.out.println("我的座驾是奥迪");
+    }
+}
---
+++ src/main/java/factoryMode/domain/Car.java
@@ -0,0 +1,8 @@
+package factoryMode.domain;
+
+/**
+ * Created by wangxc on 2017/8/19 13:12.
+ */
+public interface Car {
+    void run();
+}
