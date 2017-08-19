18fd5d2502e6a0155d31400677ac9d4f2ee38077
xinchao.wang <wangxinchao.ok@163.com>
2017-08-19 17:24:14 +0800

    测试构造者模式

--- src/main/java/builderMode/Client1.java
+++ src/main/java/builderMode/Client1.java
@@ -1,9 +1,16 @@
 package builderMode;
 
+import builderMode.domain.AirShip;
+import builderMode.domain.YellowAirShipBuilder;
+import builderMode.domain.YellowAirShipDirector;
+
 /**
  * Created by wangxc on 2017/8/19 15:44.
  */
 public class Client1 {
     public static void main(String[] args) {
+        YellowAirShipDirector yellowAirShipDirector = new YellowAirShipDirector(new YellowAirShipBuilder());
+        AirShip airShip = yellowAirShipDirector.createAirShip();
+        System.out.println("构建飞船成功");
     }
 }
