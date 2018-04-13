4727a12c3a8394a332a5ced0379706b9fe3937ba
wangxinchao <wagnxinchao@163.com>
2018-04-13 17:05:41 +0800

    n s

--- src/main/java/com/wxc/builderMode/Client1.java
+++ src/main/java/com/wxc/builderMode/Client1.java
@@ -1,16 +1,16 @@
 package com.wxc.builderMode;
 
+<<<<<<< HEAD:src/main/java/com/wxc/builderMode/Client1.java
 import com.wxc.builderMode.domain.AirShip;
 import com.wxc.builderMode.domain.YellowAirShipBuilder;
 import com.wxc.builderMode.domain.YellowAirShipDirector;
 
+=======
+>>>>>>> parent of 18fd5d2... 测试构造者模式:src/main/java/builderMode/Client1.java
 /**
  * Created by wangxc on 2017/8/19 15:44.
  */
 public class Client1 {
     public static void main(String[] args) {
-        YellowAirShipDirector yellowAirShipDirector = new YellowAirShipDirector(new YellowAirShipBuilder());
-        AirShip airShip = yellowAirShipDirector.createAirShip();
-        System.out.println("构建飞船成功");
     }
 }
